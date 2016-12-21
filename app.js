9ae17b534182647e8b5d315f26917bf8cd46d776
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-12-21 23:09:29 +0300

    initial content prop support added (for standalone)

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const model = require('./model')
 const component = require('./src/components/main')
 
 // creates choo app and appends it's tree to given element
-module.exports = (element, props) => {
+module.exports = (element, props = {}) => {
   const app = choo()
   const view = (state, prev, send) => component(state.mdEditor, send, props)
+  if (props.content) {
+    model.state.content = props.content
+  }
   app.model(model)
   app.router(route => [route('/', view)])
   const tree = app.start()
