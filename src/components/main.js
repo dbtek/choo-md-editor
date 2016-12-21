afb74649f8d9798d5a3f7f2b6d05b384ccd3c0c7
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-12-21 17:26:50 +0300

    default props added

--- src/components/main.js
+++ src/components/main.js
@@ -2,7 +2,7 @@ const html = require('choo/html')
 const editor = require('./editor')
 const toolbar = require('./toolbar')
 
-module.exports = (state, send, props) => html `
+module.exports = (state, send, props = {}) => html `
   <div class="md-editor">
     ${toolbar(state, send)}
     ${editor(state, send, props)}
